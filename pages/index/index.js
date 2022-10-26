85fff4f6dc955fde880c83f6a138e75b76744853
pfdiy <pfdiy@163.com>
2022-10-27 01:30:34 +0800

    `fix`调整样式
    `fix`调整样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,25 @@ const app = getApp();
 
 Page({
   data: {
-    active:"home"
+    active:"home",
   },
   onChange(event) {
     this.setData({ active: event.detail });
   },
+  onPullDownRefresh: function () {
+    //调用刷新时将执行的方法
+    wx.stopPullDownRefresh({
+      success: (res) => {
+        wx.showToast({
+          title: '刷新成功',
+          icon: 'none',
+          duration: 2000
+        })
+
+      },
+    })
+},
+
   onLoad: function() {},
   search: function() {
     wx.navigateTo({
