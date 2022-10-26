5946e962989f050599a0a301ceafac85d33fbd74
pfdiy <pfdiy@163.com>
2022-10-26 18:23:22 +0800

    `add` goods-card
    `add` goods-card

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,9 +2,11 @@ const app = getApp();
 
 Page({
   data: {
- 
+    active:"home"
   },
-
+  onChange(event) {
+    this.setData({ active: event.detail });
+  },
   onLoad: function() {},
   search: function() {
     wx.navigateTo({
